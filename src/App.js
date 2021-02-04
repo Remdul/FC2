323b080767fba129c8c6a75727937aca1bee32d5
EC2 Default User <ec2-user@ip-172-31-6-221.us-east-2.compute.internal>
2021-02-04 16:57:33 +0000

    Add Authentication 2.0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-import React from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-
-const App = () => (
-  <div>
-    <AmplifySignOut />
-    My App
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
+}
 
 export default withAuthenticator(App);
